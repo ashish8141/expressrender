827128b49f2445866e78f7d67c55150c8d3a716f
Ashish Jat <44964400+ashish8141@users.noreply.github.com>
2023-07-08 00:42:37 +0530

    Post Query

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const mailWizz = require("node-mailwizz");
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3030;
 
 app.get("/", (req, res) => {
     res.send('Success')
 });
+
+app.post("/", (req,rest) => {
+    console.log('Got body:', req.body);
+    rest.sendStatus(200);
+})
 
 app.listen(PORT, () => console.log("Server listening on port 3000!"));
